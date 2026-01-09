0ee1455b1af40e2a9e18ed077c3993e9cb048276
I561104 <f.vandenheuvel@student.fontys.nl>
2026-01-09 21:51:55 +0100

    fixed security issues

--- core/interface/api/urls.py
+++ core/interface/api/urls.py
@@ -1,15 +1,26 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core.interface.api.views.auth import register
 from core.interface.api.views.profile import me, my_reviews
-from core.interface.api.views.reviews import review_delete, reviews
+from core.interface.api.views.reviews import (
+    review_delete,
+    reviews_create,
+    reviews_list,
+)
 
 urlpatterns = [
-    path("reviews/", reviews, name="reviews"),
-    path("reviews/<int:pk>/", review_delete, name="review_delete"),
+    # Reviews
+    path("reviews/", reviews_list, name="reviews-list"),
+    path("reviews/new/", reviews_create, name="reviews-create"),
+    path("reviews/<int:pk>/", review_delete, name="review-delete"),
+    # Profile
     path("me/", me, name="me"),
-    path("my-reviews/", my_reviews, name="my_reviews"),
+    path("my-reviews/", my_reviews, name="my-reviews"),
+    # Auth
     path("register/", register, name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
--- core/interface/api/views/reviews.py
+++ core/interface/api/views/reviews.py
@@ -1,4 +1,8 @@
-from django.views.decorators.http import require_http_methods, require_POST
+from django.views.decorators.http import (
+    require_GET,
+    require_http_methods,
+    require_POST,
+)
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
@@ -12,33 +16,45 @@ from core.application.services.reviews_service import (
 from core.interface.api.serializers import ReviewSerializer
 
 
-@api_view(["GET", "POST"])
+@require_GET
+@api_view(["GET"])
 @permission_classes([IsAuthenticatedOrReadOnly])
-@require_http_methods(["GET", "POST"])
-def reviews(request):
-    if request.method == "GET":
-        artist_id = request.GET.get("artist_id")
-        album_id = request.GET.get("album_id")
-        queryset = get_reviews(artist_id=artist_id, album_id=album_id)
-        return Response(
-            ReviewSerializer(queryset, many=True).data, status=status.HTTP_200_OK
-        )
+def reviews_list(request):
+    artist_id = request.GET.get("artist_id")
+    album_id = request.GET.get("album_id")
+    queryset = get_reviews(artist_id=artist_id, album_id=album_id)
+    return Response(
+        ReviewSerializer(queryset, many=True).data,
+        status=status.HTTP_200_OK,
+    )
 
+
+@require_POST
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def reviews_create(request):
     serializer = ReviewSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
+
     review = create_new_review(
-        user=request.user, validated_data=serializer.validated_data
+        user=request.user,
+        validated_data=serializer.validated_data,
     )
-    return Response(ReviewSerializer(review).data, status=status.HTTP_201_CREATED)
+    return Response(
+        ReviewSerializer(review).data,
+        status=status.HTTP_201_CREATED,
+    )
 
 
+@require_http_methods(["DELETE"])
 @api_view(["DELETE"])
 @permission_classes([IsAuthenticated])
-@require_POST
 def review_delete(request, pk):
     _, err = delete_review_as_admin(user=request.user, pk=pk)
+
     if err == "Not allowed":
         return Response({"detail": err}, status=status.HTTP_403_FORBIDDEN)
     if err == "Not found":
         return Response({"detail": err}, status=status.HTTP_404_NOT_FOUND)
+
     return Response(status=status.HTTP_204_NO_CONTENT)
